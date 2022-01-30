c3b8eacfe94cd68d5a7112ec0e067a98b97f68ac
wholespace <wholespace.214@gmail.com>
2022-01-30 22:00:56 +0900

    about update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,18 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Electron', 'Node.js', 'BlockChain'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Electron',
+    'Node.js',
+    'BlockChain',
+    'Vue.js',
+    'Angular',
+    'Next.js',
+    'Figma',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,33 +147,27 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Naruhito and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Naruhito and Full Stack developer.
+              <br />
+              REACT | Angular | Node.js | MEAN stack | REACT Native | PHP | Laravel | MongoDB |
+              Android | WordPress plugins Development, Social media API expert with and 9+ years of
+              web application development experience.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I believe in client satisfaction. I need an opportunity to prove my skills. Just a
+              talk and we can have a better understanding and can work together for a long time.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I have also working experience with Model View Controller (MVC) and have been involved
+              in maintaining versions of source code using{' '}
+              <a href="https://github.com/wholespace">GIT</a>. I have also designed and developed
+              web user controls, pages, validation controls, CSS files using technologies like AJAX
+              Toolkit, jQuery, JavaScript, Bootstrap UI controls, XML, HTML, and DHTML.
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Qualifications and experience:</p>
           </div>
 
           <ul className="skills-list">
